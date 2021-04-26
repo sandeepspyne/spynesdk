cec2e874f4543fe20eab9717097d061ef4d77348
sandeepspyne <sandeep.singh@spyne.ai>
2021-04-26 20:49:21 +0530

    new ui added

---
+++ SpyneAi/app/src/main/java/com/spyneai/videorecording/ThreeSixtyViewTestActivity.kt
@@ -0,0 +1,12 @@
+package com.spyneai.videorecording
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spyneai.R
+
+class ThreeSixtyViewTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three_sixty_view_test)
+    }
+}
