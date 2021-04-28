b41f848ec97f8412c58f00a085df3cff2c157129
gouravspyne <gourav.dhankher@spyne.ai>
2021-04-28 08:13:02 +0530

    start working on viewpager2 for completed and ongoing screen

---
+++ SpyneAi/app/src/main/java/com/spyneai/activity/YourOrders.kt
@@ -0,0 +1,12 @@
+package com.spyneai.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spyneai.R
+
+class YourOrders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_orders2)
+    }
+}
