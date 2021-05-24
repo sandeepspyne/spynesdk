9fbacb0e28640f361d05936c72f471326c46a4e9
gouravspyne <gourav.dhankher@spyne.ai>
2021-05-24 20:08:28 +0530

    bottom navigation shoot button added

--- SpyneAi/app/src/main/java/com/spyneai/dashboard/ui/dashboard/MainDashboardActivity.kt
+++ SpyneAi/app/src/main/java/com/spyneai/dashboard/ui/dashboard/MainDashboardActivity.kt
@@ -1,5 +1,6 @@
 package com.spyneai.dashboard.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.spyneai.R
+import com.spyneai.activity.CategoriesActivity
 import kotlinx.android.synthetic.main.activity_dashboard_main.*
 
 
@@ -25,9 +27,16 @@ class MainDashboardActivity : AppCompatActivity() {
 
         navController = navHostFragment.navController
 
-
-
+        bottomNavigation.background = null
+        bottomNavigation.menu.getItem(2).isEnabled = false
         bottomNavigation.setupWithNavController(navController)
+
+//        fab.setImageDrawable(this, R.drawable.ic_shoot)
+
+        fab.setOnClickListener {
+            val intent = Intent(this, CategoriesActivity::class.java)
+            startActivity(intent)
+        }
 
 //        NavigationUI.setupActionBarWithNavController(this, navController)
     }
