cedf79a827274b5eaf58e9e719d45091a63fa486
Gourav Dhankher <gourav@Gouravs-MacBook-Air.local>
2021-06-11 14:37:50 +0530

    camera issue resolved

--- SpyneAi/app/src/main/java/com/spyneai/shoot/ui/CameraFragment.kt
+++ SpyneAi/app/src/main/java/com/spyneai/shoot/ui/CameraFragment.kt
@@ -1,12 +1,10 @@
 package com.spyneai.shoot.ui
 
 import android.app.Activity
-import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.*
-import android.provider.MediaStore
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,21 +13,19 @@ import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import com.bumptech.glide.Glide
-import com.robertlevonyan.demo.camerax.analyzer.LuminosityAnalyzer
+import androidx.fragment.app.DialogFragment
+import com.balsikandar.kotlindslsamples.dialogfragment.DialogDSLBuilder.Companion.dialog
+import com.spyneai.R
 import com.spyneai.base.BaseFragment
-import com.spyneai.base.network.Resource
-import com.spyneai.dashboard.response.NewSubCatResponse
-import com.spyneai.dashboard.ui.handleApiError
 import com.spyneai.databinding.FragmentCameraBinding
-import com.spyneai.needs.AppConstants
 import com.spyneai.shoot.data.model.ShootData
-import com.spyneai.shoot.utils.ThreadExecutor
-import com.spyneai.shoot.utils.mainExecutor
 import kotlinx.android.synthetic.main.activity_camera.*
+import kotlinx.android.synthetic.main.dialog_confirm_reshoot.view.*
 import java.io.File
-import java.util.ArrayList
-import java.util.concurrent.ExecutionException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -41,9 +37,15 @@ class CameraFragment : BaseFragment<ShootViewModel, FragmentCameraBinding>() {
     private var preview: Preview? = null
     private var imageAnalyzer: ImageAnalysis? = null
 
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var outputDirectory: File
+
+    private val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
     private var projectId: String = "prj-27d33afa-4f50-4af0-b769-a97adf247fae"
     private var skuId: String = "sku-9c0775d2-69e4-4ecf-a134-7b61a48e15ee\n"
-    private var imageCategory: String= "Exterior"
+    private var imageCategory: String = "Exterior"
     private var authKey: String = "813a71af-a2fb-4ef8-87b3-059d01c5b9ba"
 
     // Selector showing which camera is selected (front or back)
@@ -56,16 +58,25 @@ class CameraFragment : BaseFragment<ShootViewModel, FragmentCameraBinding>() {
         private const val RATIO_16_9_VALUE = 16.0 / 9.0 // aspect ratio 16x9
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        // Determine the output directory
+        outputDirectory = ShootActivity.getOutputDirectory(requireContext())
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         startCamera()
-        binding.btnTakePicture?.setOnClickListener { captureImage() }
+        binding.btnTakePicture?.setOnClickListener { takePhoto() }
 
         binding.ivUploadedImage?.setOnClickListener {
             binding.ivUploadedImage!!.visibility = View.GONE
         }
 
     }
+
 
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
         super.setUserVisibleHint(isVisibleToUser)
@@ -75,70 +86,35 @@ class CameraFragment : BaseFragment<ShootViewModel, FragmentCameraBinding>() {
         }
     }
 
-    //The folder location where all the files will be stored
-    protected val outputDirectory: String by lazy {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            "${Environment.DIRECTORY_DCIM}/Spyne/"
-        } else {
-            "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)}/Spyne/"
-        }
-    }
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
 
-    private fun captureImage() {
-        val localImageCapture =
-            imageCapture ?: throw IllegalStateException("Camera initialization failed.")
 
-        // Setup image capture metadata
-        val metadata = ImageCapture.Metadata().apply {
-            // Mirror image when using the front camera
-            isReversedHorizontal = lensFacing == CameraSelector.DEFAULT_FRONT_CAMERA
-        }
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
 
-        // Options fot the output image file
-        val outputOptions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val contentValues = ContentValues().apply {
-                put(MediaStore.MediaColumns.DISPLAY_NAME, System.currentTimeMillis())
-                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-                put(MediaStore.MediaColumns.RELATIVE_PATH, outputDirectory)
-            }
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-            val contentResolver = requireContext().contentResolver
-
-            // Create the output uri
-            val contentUri =
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-
-            ImageCapture.OutputFileOptions.Builder(contentResolver, contentUri, contentValues)
-        } else {
-            File(outputDirectory).mkdirs()
-            val file = File(outputDirectory, "${System.currentTimeMillis()}.jpg")
-
-            ImageCapture.OutputFileOptions.Builder(file)
-        }.setMetadata(metadata).build()
-
-        localImageCapture.takePicture(
-            outputOptions, // the options needed for the final image
-            requireContext().mainExecutor(), // the executor, on which the task will run
-            object :
-                ImageCapture.OnImageSavedCallback { // the callback, about the result of capture process
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    // This function is called if capture is successfully completed
-                    outputFileResults.savedUri
-                        ?.let { uri ->
-                            viewModel.shootList.value?.add(ShootData(uri, "prj-27d33afa-4f50-4af0-b769-a97adf247fae",
-                                "sku-9c0775d2-69e4-4ecf-a134-7b61a48e15ee", "Exterior", "813a71af-a2fb-4ef8-87b3-059d01c5b9ba"))
-                        }
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireContext()), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
-
-                override fun onError(exception: ImageCaptureException) {
-                    // This function is called if there is an errors during capture process
-                    val msg = "Photo capture failed: ${exception.message}"
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
                     Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
-                    Log.e(TAG, msg)
-                    exception.printStackTrace()
+                    Log.d(TAG, msg)
+                    viewModel.shootList.value?.add(ShootData(savedUri, "prj-27d33afa-4f50-4af0-b769-a97adf247fae",
+                                "sku-9c0775d2-69e4-4ecf-a134-7b61a48e15ee", "Exterior", "813a71af-a2fb-4ef8-87b3-059d01c5b9ba"))
                 }
-            }
-        )
+            })
     }
 
     private fun aspectRatio(width: Int, height: Int): Int {
@@ -150,86 +126,47 @@ class CameraFragment : BaseFragment<ShootViewModel, FragmentCameraBinding>() {
     }
 
     private fun startCamera() {
-        // This is the CameraX PreviewView where the camera will be rendered
-        val viewFinder = binding.viewFinder
-
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-        cameraProviderFuture.addListener({
-            try {
-                cameraProvider = cameraProviderFuture.get()
-            } catch (e: InterruptedException) {
-                Toast.makeText(requireContext(), "Error starting camera", Toast.LENGTH_SHORT).show()
-                return@addListener
-            } catch (e: ExecutionException) {
-                Toast.makeText(requireContext(), "Error starting camera", Toast.LENGTH_SHORT).show()
-                return@addListener
-            }
 
-            // The display information
-            val metrics = DisplayMetrics().also { viewFinder?.display?.getRealMetrics(it) }
-            // The ratio for the output image and preview
-            val aspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
-            // The display rotation
-            val rotation = viewFinder?.display?.rotation
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val localCameraProvider = cameraProvider
-                ?: throw IllegalStateException("Camera initialization failed.")
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
 
-            // The Configuration of camera preview
-            preview = rotation?.let {
-                Preview.Builder()
-                    .setTargetAspectRatio(aspectRatio) // set the camera aspect ratio
-                    .setTargetRotation(it) // set the camera rotation
-                    .build()
-            }
+            imageCapture = ImageCapture.Builder()
+                .build()
 
-            // The Configuration of image capture
-            imageCapture = rotation?.let {
-                ImageCapture.Builder()
-                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY) // setting to have pictures with highest quality possible (may be slow)
-                    .setTargetAspectRatio(aspectRatio) // set the capture aspect ratio
-                    .setTargetRotation(it) // set the capture rotation
-                    .build()
-            }
-
-            // The Configuration of image analyzing
-            imageAnalyzer = rotation?.let {
-                ImageAnalysis.Builder()
-                    .setTargetAspectRatio(aspectRatio) // set the analyzer aspect ratio
-                    .setTargetRotation(it) // set the analyzer rotation
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST) // in our analysis, we care about the latest image
-                    .build()
-                    .apply {
-                        // Use a worker thread for image analysis to prevent glitches
-                        val analyzerThread = HandlerThread("LuminosityAnalysis").apply { start() }
-                        setAnalyzer(
-                            ThreadExecutor(Handler(analyzerThread.looper)),
-                            LuminosityAnalyzer()
-                        )
-                    }
-            }
-
-            localCameraProvider.unbindAll() // unbind the use-cases before rebinding them
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
-                // Bind all use cases to the camera with lifecycle
-                localCameraProvider.bindToLifecycle(
-                    viewLifecycleOwner, // current lifecycle owner
-                    lensFacing, // always back facing
-                    preview, // camera preview use case
-                    imageCapture, // image capture use case
-                    imageAnalyzer, // image analyzer use case
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    viewLifecycleOwner, cameraSelector, preview, imageCapture
                 )
 
-                // Attach the viewfinder's surface provider to preview use case
-                if (viewFinder != null) {
-                    preview?.setSurfaceProvider(viewFinder.surfaceProvider)
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to bind use cases", e)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
 
         }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // Shut down our background executor
+        cameraExecutor.shutdown()
+
     }
 
 
--- SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ShootActivity.kt
+++ SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ShootActivity.kt
@@ -2,6 +2,7 @@ package com.spyneai.shoot.ui
 
 import android.Manifest
 import android.app.FragmentManager
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -27,6 +28,7 @@ import kotlinx.android.synthetic.main.dialog_gif_hint.view.*
 import kotlinx.android.synthetic.main.dialog_interior_shoot.view.*
 import kotlinx.android.synthetic.main.dialog_interior_shoot.view.btShootInterior
 import kotlinx.android.synthetic.main.dialog_shoot_hint.view.*
+import java.io.File
 
 
 class ShootActivity : AppCompatActivity() {
@@ -177,6 +179,15 @@ class ShootActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
 
-
+        /** Use external media if it is available, our app's file directory otherwise */
+        fun getOutputDirectory(context: Context): File {
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() } }
+            return if (mediaDir != null && mediaDir.exists())
+                mediaDir else appContext.filesDir
+        }
+    }
 }
