9b8725a0ecaeb96f44ff359dcaa410c430a39af2
gouravspyne <gourav.dhankher@spyne.ai>
2021-06-30 18:48:29 +0530

    create project and sku api integration in process

--- SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ecom/CreateProjectEcomDialog.kt
+++ SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ecom/CreateProjectEcomDialog.kt
@@ -1,13 +1,82 @@
 package com.spyneai.shoot.ui.ecom
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.posthog.android.Properties
 import com.spyneai.base.BaseDialogFragment
+import com.spyneai.base.network.Resource
+import com.spyneai.captureEvent
+import com.spyneai.captureFailureEvent
+import com.spyneai.dashboard.ui.handleApiError
 import com.spyneai.databinding.CreateProjectEcomDialogBinding
+import com.spyneai.needs.AppConstants
+import com.spyneai.needs.Utilities
+import com.spyneai.posthog.Events
 import com.spyneai.shoot.data.ShootViewModel
+import com.spyneai.shoot.data.model.Sku
 
 class CreateProjectEcomDialog : BaseDialogFragment<ShootViewModel, CreateProjectEcomDialogBinding>() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.setCancelable(false)
+
+        binding.btnProceed.setOnClickListener {
+            when {
+                binding.etProjectName.text.toString().isEmpty() -> binding.etProjectName.error = "Please enter project name"
+                binding.etSkuName.text.toString().isEmpty() -> {
+                    binding.etSkuName.error = "Please enter product name"
+                }
+                else -> {
+                    createProject(binding.etProjectName.text.toString(),binding.etSkuName.text.toString())
+                }
+            }
+        }
+    }
+
+    private fun createProject(projectName : String,skuName : String) {
+        viewModel.createProject(
+            Utilities.getPreference(requireContext(), AppConstants.AUTH_KEY).toString(),
+            projectName,
+            requireActivity().intent.getStringExtra(AppConstants.CATEGORY_ID).toString())
+
+        viewModel.createProjectRes.observe(viewLifecycleOwner,{
+            when(it){
+                is Resource.Sucess -> {
+                    requireContext().captureEvent(
+                        Events.CREATE_PROJECT,
+                        Properties().putValue("project_name",projectName))
+
+                    Utilities.hideProgressDialog()
+                    //notify project created
+                    viewModel.isProjectCreated.value = true
+                    val sku = Sku()
+                    sku.projectId = it.value.project_id
+                    sku.skuName = skuName
+                    viewModel.sku.value = sku
+
+                    dismiss()
+                }
+
+                is Resource.Loading -> {
+                    Utilities.showProgressDialog(requireContext())
+                }
+
+                is Resource.Failure -> {
+                    requireContext().captureFailureEvent(
+                        Events.CREATE_PROJECT_FAILED, Properties(),
+                        it.errorMessage!!
+                    )
+                    Utilities.hideProgressDialog()
+                    handleApiError(it)
+                }
+            }
+        })
+
+    }
 
 
     override fun getViewModel() = ShootViewModel::class.java
--- SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ecom/OverlaysEcomFragment.kt
+++ SpyneAi/app/src/main/java/com/spyneai/shoot/ui/ecom/OverlaysEcomFragment.kt
@@ -10,11 +10,46 @@ import com.spyneai.R
 import com.spyneai.base.BaseFragment
 import com.spyneai.databinding.FragmentOverlaysEcomBinding
 import com.spyneai.shoot.data.ShootViewModel
+import com.spyneai.shoot.data.model.ShootData
+import com.spyneai.shoot.ui.dialogs.ConfirmReshootDialog
+import com.spyneai.shoot.ui.dialogs.CreateProjectAndSkuDialog
 
 class OverlaysEcomFragment : BaseFragment<ShootViewModel, FragmentOverlaysEcomBinding>() {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        //observe new image clicked
+//        viewModel.shootList.observe(viewLifecycleOwner, {
+//            try {
+//                if (showDialog && !it.isNullOrEmpty()){
+//                    showImageConfirmDialog(it.get(it.size - 1))
+//                }
+//            }catch (e : Exception){
+//                e.printStackTrace()
+//            }
+//        })
+
+
+//        private fun initProjectDialog(){
+//            CreateProjectAndSkuDialog().show(requireFragmentManager(), "CreateProjectAndSkuDialog")
+//
+//            viewModel.isProjectCreated.observe(viewLifecycleOwner,{
+//                if (it) {
+//                    intSubcategorySelection()
+//                }
+//            })
+//
+//        }
+//
+//        private fun showImageConfirmDialog(shootData: ShootData) {
+//            viewModel.shootData.value = shootData
+//            ConfirmReshootDialog().show(requireFragmentManager(), "ConfirmReshootDialog")
+//        }
+//
+    }
 
     override fun getViewModel() = ShootViewModel::class.java
 
