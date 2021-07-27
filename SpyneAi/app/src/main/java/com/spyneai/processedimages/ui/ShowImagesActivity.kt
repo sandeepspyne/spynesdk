10a06a7b3a5e2f0ba4dc8d322fcedd539eab4105
sandeepspyne <sandeep.singh@spyne.ai>
2021-07-27 20:24:44 +0530

    360 exterior view added for regular shoot

--- SpyneAi/app/src/main/java/com/spyneai/processedimages/ui/ShowImagesActivity.kt
+++ SpyneAi/app/src/main/java/com/spyneai/processedimages/ui/ShowImagesActivity.kt
@@ -2,8 +2,11 @@ package com.spyneai.processedimages.ui
 
 import android.app.*
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -12,10 +15,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.NotificationCompat
+import androidx.core.view.MotionEventCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.downloader.*
 import com.spyneai.R
 import com.spyneai.activity.DownloadingActivity
@@ -35,6 +43,8 @@ import com.spyneai.needs.AppConstants
 import com.spyneai.needs.ScrollingLinearLayoutManager
 import com.spyneai.needs.Utilities
 import com.spyneai.shoot.utils.log
+import com.spyneai.videorecording.fragments.DialogEmbedCode
+import com.spyneai.videorecording.model.TSVParams
 import com.spyneai.videorecording.model.VideoProcessingResponse
 
 import com.spyneai.videorecording.service.FramesHelper
@@ -52,7 +62,12 @@ import retrofit2.Response
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ShowImagesActivity : AppCompatActivity() {
+class ShowImagesActivity : AppCompatActivity(),View.OnTouchListener,View.OnClickListener  {
+    private lateinit var frontFramesList: ArrayList<String>
+    lateinit var tsvParamFront : TSVParams
+    var handler = Handler()
+    var shootId = ""
+
     lateinit var builder: NotificationCompat.Builder
     lateinit var imageList: List<String>
     lateinit var imageListAfter: List<String>
@@ -81,7 +96,9 @@ class ShowImagesActivity : AppCompatActivity() {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        frontFramesList = ArrayList()
         setBulkImages()
+
 
         setListeners()
 
@@ -215,6 +232,9 @@ class ShowImagesActivity : AppCompatActivity() {
             orderIntent.putExtra(AppConstants.IMAGE_TYPE,intent.getStringExtra(AppConstants.IMAGE_TYPE))
             startActivity(orderIntent)
         }
+
+        ivShare.setOnClickListener(this)
+        ivEmbed.setOnClickListener(this)
     }
 
 
@@ -286,14 +306,18 @@ class ShowImagesActivity : AppCompatActivity() {
 
     //Fetch bulk data
     private fun fetchBulkUpload() {
+
+        shootId = "sku-a5d2fb30-2878-4c3c-b757-c1fb2b02c755"
+        //shootId = Utilities.getPreference(this, AppConstants.SKU_ID)!!
+
         val request = RetrofitClients.buildService(APiService::class.java)
         val authKey = RequestBody.create(
             MultipartBody.FORM,
-            Utilities.getPreference(this, AppConstants.AUTH_KEY)!!
+           "fd95f78c-9ff1-4bcc-a289-a9173d820ff1"
         )
         val skuId = RequestBody.create(
             MultipartBody.FORM,
-            Utilities.getPreference(this, AppConstants.SKU_ID)!!
+            shootId
         )
 
         val call = request.fetchBulkImageV2(skuId, authKey)
@@ -323,6 +347,7 @@ class ShowImagesActivity : AppCompatActivity() {
                             (listHdQuality as ArrayList).add(dataList!![i].output_image_hres_url)
 
                             imageNameList.add(dataList[i].image_name)
+                            frontFramesList.add(dataList!![i].output_image_lres_url)
 
                             Utilities.savePrefrence(
                                 this@ShowImagesActivity,
@@ -374,6 +399,11 @@ class ShowImagesActivity : AppCompatActivity() {
                         }
                     }
                 }
+
+
+                //show 360 view
+                showThreeSixtyView()
+
                 showReplacedImagesAdapter.notifyDataSetChanged()
                 ShowReplacedImagesInteriorAdapter.notifyDataSetChanged()
                 ShowReplacedImagesFocusedAdapter.notifyDataSetChanged()
@@ -388,6 +418,58 @@ class ShowImagesActivity : AppCompatActivity() {
                 ).show()
             }
         })
+    }
+
+    private fun showThreeSixtyView() {
+//        frontFramesList = it.value.data.map { it.output_image_lres_url }
+
+        tsvParamFront = TSVParams()
+        tsvParamFront.type = 0
+        tsvParamFront.framesList = frontFramesList
+        tsvParamFront.mImageIndex = frontFramesList.size / 2
+
+        sv_front.startShimmer()
+
+        preLoadFront(tsvParamFront)
+
+        //load front image
+        Glide.with(this)
+            .load(frontFramesList.get(tsvParamFront.mImageIndex))
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    iv_front.visibility = View.VISIBLE
+                    sv_front.stopShimmer()
+                    sv_front.visibility = View.GONE
+
+                    //show images and set listener
+                   cl_front.visibility = View.VISIBLE
+
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    iv_front.visibility = View.VISIBLE
+                    sv_front.stopShimmer()
+                    sv_front.visibility = View.GONE
+
+                    //show images and set listener
+                    cl_front.visibility = View.VISIBLE
+                    return false
+                }
+
+            })
+            .into(iv_front)
     }
 
     fun showImagesDialog(position: Int) {
@@ -433,5 +515,204 @@ class ShowImagesActivity : AppCompatActivity() {
 
             return customView
         }
+    }
+
+
+
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+        var action = MotionEventCompat.getActionMasked(event)
+
+        when(v?.id){
+            R.id.iv_front -> {
+                when(action){
+                    MotionEvent.ACTION_DOWN -> {
+                        tsvParamFront.mStartX = event!!.x.toInt()
+                        tsvParamFront.mStartY = event.y.toInt()
+                        return true
+                    }
+
+                    MotionEvent.ACTION_MOVE -> {
+                        tsvParamFront.mEndX = event!!.x.toInt()
+                        tsvParamFront.mEndY = event.y.toInt()
+
+                        if (tsvParamFront.mEndX - tsvParamFront.mStartX > 3) {
+                            tsvParamFront.mImageIndex++
+                            if (tsvParamFront.mImageIndex >= tsvParamFront.framesList.size) tsvParamFront.mImageIndex = 0
+
+                            loadImage(tsvParamFront,iv_front)
+
+                        }
+                        if (tsvParamFront.mEndX - tsvParamFront.mStartX < -3) {
+                            tsvParamFront.mImageIndex--
+                            if (tsvParamFront.mImageIndex < 0) tsvParamFront.mImageIndex = tsvParamFront.framesList.size - 1
+
+                            loadImage(tsvParamFront,iv_front)
+                        }
+                        tsvParamFront.mStartX = event.x.toInt()
+                        tsvParamFront.mStartY = event.y.toInt()
+
+                        return true
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        tsvParamFront.mEndX = event!!.x.toInt()
+                        tsvParamFront.mEndY = event.y.toInt()
+
+                        return true
+                    }
+                    MotionEvent.ACTION_CANCEL -> return true
+                    MotionEvent.ACTION_OUTSIDE -> return true
+                }
+            }
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+
+            R.id.ivEmbed -> {
+                embed(getCode(0))
+            }
+
+            R.id.ivShare -> {
+                share(getLink())
+            }
+
+            R.id.tv_go_to_home -> {
+                gotoHome()
+            }
+        }
+    }
+
+    private fun embed(code: String) {
+        var args = Bundle()
+        args.putString("code",code)
+
+        var dialogCopyEmbeddedCode = DialogEmbedCode()
+        dialogCopyEmbeddedCode.arguments = args
+        dialogCopyEmbeddedCode.show(supportFragmentManager,"DialogEmbedCode")
+    }
+
+    private fun share(code: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, code)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    private fun getCode(type : Int) : String {
+        return "<iframe \n" +
+                "  src=\"https://www.spyne.ai/shoots/shoot?skuId="+shootId+"&type=360_exterior" +
+                "  style=\"border:0; height: 100%; width: 100%;\" framerborder=\"0\"></iframe>"
+
+    }
+
+    private fun getLink() = "https://www.spyne.ai/shoots/shoot?skuId="+shootId+"&type=exterior"
+
+    private fun preLoadFront(tsvParams: TSVParams) {
+        for ((index, url) in tsvParams.framesList.withIndex()) {
+
+            Glide.with(this)
+                .load(url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.d(TAG, "onResourceReady: failed")
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.d(TAG, "onResourceReady: paseed " + index)
+
+
+                        if (index == tsvParams.framesList.size - 1) {
+
+                            iv_front.setOnTouchListener(this@ShowImagesActivity)
+                        }
+
+                        return false
+                    }
+
+                })
+                .dontAnimate()
+                .override(250, 250)
+                .preload()
+
+        }
+
+        setListeners()
+    }
+
+    private fun loadImage(tsvParams: TSVParams, imageView: ImageView) {
+
+        handler.removeCallbacksAndMessages(null)
+
+        handler.postDelayed({
+
+            Log.d(TAG, "loading: a"+tsvParams.type)
+            Log.d(TAG, "loading: a"+tsvParams.framesList.get(tsvParams.mImageIndex))
+
+
+            try {
+                var glide = Glide.with(this)
+                    .load(tsvParams.framesList.get(tsvParams.mImageIndex))
+
+                if (tsvParams.placeholder != null)
+                    glide.placeholder(tsvParams.placeholder)
+
+                glide.listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.d(TAG, "onResourceReady: failed")
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        tsvParams.placeholder = resource!!
+
+                        return false
+                    }
+
+                })
+                    .override(250, 250)
+                    .dontAnimate()
+                    .into(imageView)
+
+
+                if (iv_front.visibility == View.INVISIBLE)iv_front.visibility = View.VISIBLE
+            } catch (ex: UninitializedPropertyAccessException) {
+                Log.d(TAG, "loadImage: ex " + tsvParams.type)
+                Log.d(TAG, "loadImage: ex " + ex.localizedMessage)
+
+            }
+        }, 10)
     }
 }
