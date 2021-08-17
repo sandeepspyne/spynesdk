9109016b38551deca4a928d8feb70313a03bf051
gouravspyne <gourav.dhankher@spyne.ai>
2021-08-17 13:17:11 +0530

    working on lal10

---
+++ SpyneAi/app/src/lal10/java/com/spyneai/dashboard/ui/WhitelableConstants.kt
@@ -0,0 +1,9 @@
+package com.spyneai.dashboard.ui
+
+object WhiteLabelConstants {
+    val API_KEY: String = "2e9141ef-79d8-4891-b26f-bb7dfc93b45d"
+    val ENTERPRISE_ID: String = "LZEGE6S28"
+    val KARVI = "Karvi Studio"
+    val CARS24_INDIA = "Cars24 India"
+    val CARS24= "Cars24"
+}
