632fbe080cff99aab0de1a14562a5391a9ab3d72
gouravspyne <gourav.dhankher@spyne.ai>
2021-08-17 16:49:59 +0530

    enterprise ids updated

--- SpyneAi/app/src/udaan/java/com/spyneai/dashboard/ui/WhiteLabelConstants.kt
+++ SpyneAi/app/src/udaan/java/com/spyneai/dashboard/ui/WhiteLabelConstants.kt
@@ -1,8 +1,8 @@
 package com.spyneai.dashboard.ui
 
 object WhiteLabelConstants {
-    val API_KEY: String = "2e9141ef-79d8-4891-b26f-bb7dfc93b45d"
-    val ENTERPRISE_ID: String = "LZEGE6S28"
+    val API_KEY: String = "15d2488b-732a-43cc-b7f7-48c6326621eb"
+    val ENTERPRISE_ID: String = "DGHK9YZ0Q"
     val KARVI = "Karvi Studio"
     val CARS24_INDIA = "Cars24 India"
     val CARS24= "Cars24"
