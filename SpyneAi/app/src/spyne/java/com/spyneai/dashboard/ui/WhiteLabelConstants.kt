7189671b1fe45eb7af4ef8b378cf8b677169bfb2
sandeepspyne <sandeep.singh@spyne.ai>
2021-08-27 13:27:21 +0530

    spyne app added

---
+++ SpyneAi/app/src/spyne/java/com/spyneai/dashboard/ui/WhiteLabelConstants.kt
@@ -0,0 +1,7 @@
+package com.spyneai.dashboard.ui
+
+object WhiteLabelConstants {
+    val API_KEY: String = "ee84dc3e-340e-4d39-a24c-9365272405f9"
+    val ENTERPRISE_ID: String = "TaD1VC1Ko"
+    val KARVI = "Karvi Studio"
+}
