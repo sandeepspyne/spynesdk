3c94f21276c951b92035cba387bb39c58d97271f
gouravspyne <gourav.dhankher@spyne.ai>
2021-09-08 18:26:53 +0530

    swiggyinstamart completed

--- SpyneAi/app/src/flipkart/java/com/spyneai/dashboard/ui/WhitelableConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.spyneai.dashboard.ui
-
-object WhiteLabelConstants {
-    val API_KEY: String = "a7f4d5dc-42b1-43e3-9f9e-8ae7851ef250"
-    val ENTERPRISE_ID: String = "JZYD0R89B"
-    val KARVI = "Karvi Studio"
-    val CARS24_INDIA = "Cars24 India"
-    val CARS24= "Cars24"
-}
---
+++ SpyneAi/app/src/swiggyinstamart/java/com/spyneai/dashboard/ui/WhitelableConstants.kt
@@ -0,0 +1,9 @@
+package com.spyneai.dashboard.ui
+
+object WhiteLabelConstants {
+    val API_KEY: String = "d31729be-9bc1-457d-addf-172478b2aa4b"
+    val ENTERPRISE_ID: String = "W2CZI80P8"
+    val KARVI = "Karvi Studio"
+    val CARS24_INDIA = "Cars24 India"
+    val CARS24= "Cars24"
+}
