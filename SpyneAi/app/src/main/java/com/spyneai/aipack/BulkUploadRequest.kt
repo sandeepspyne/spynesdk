b048c7193cad0309d14bf3d08d0a87c312971e9d
sandeepspyne <sandeep.singh@spyne.ai>
2021-10-06 13:22:34 +0530

    extra retrofit clients removed

--- SpyneAi/app/src/main/java/com/spyneai/aipack/BulkUploadRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.spyneai.aipack
-
-data class BulkUploadRequest(
-    val background : List<String>,
-    val image_url : List<String>,
-    val user_id : String,
-    val sku_id : String,
-    val sku_name : String
-)
